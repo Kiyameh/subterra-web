f05b0ed900916e85fc64915cf4962b1e8dd10a0b
Kiyameh <kiyameh@outlook.com>
2025-03-13 09:47:40 +0100

    Corregir footer para responsividad

--- components/navigations/landing-footer.tsx
+++ components/navigations/landing-footer.tsx
@@ -4,61 +4,37 @@ import {SubterraLogoSm} from '@/components/branding/subterra-logo'
 
 export default function LandingFooter() {
   return (
-    <footer className="text-sm h-auto p-4 w-full bg-card flex flex-col justify-center items-center space-y-4">
-      <div className="flex justify-center space-x-6 items-center">
-        <ul className="flex flex-col justify-center items-end space-x-4">
-          <li>
-            <Link
-              href="/privacy-policy"
-              className="text-yellow-500"
-            >
-              Politica de privacidad
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/terms-of-service"
-              className="text-yellow-500"
-            >
-              Condiciones del servicio
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/cookie-policy"
-              className="text-yellow-500"
-            >
-              Politica de cookies
-            </Link>
-          </li>
-        </ul>
-        <SubterraLogoSm />
-        <ul className="flex flex-col justify-center items-start">
-          <li>
-            <Link
-              href="/user-guide"
-              className="text-yellow-500"
-            >
-              Guía de uso
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/versions"
-              className="text-yellow-500"
-            >
-              Versiones y novedades
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              className="text-yellow-500"
-            >
-              Contacto
-            </Link>
-          </li>
-        </ul>
+    <footer className="w-full h-auto p-4 bg-card flex flex-col justify-center items-center space-y-4 text-sm">
+      <div className="w-full flex flex-col items-center justify-center md:flex-row  md:justify-center md:space-x-6 text-yellow-500">
+        <div className="order-2 md:order-1">
+          <ul className="flex flex-col items-center md:items-end text-center">
+            <li>
+              <Link href="/privacy-policy">Politica de privacidad</Link>
+            </li>
+            <li>
+              <Link href="/terms-of-service">Condiciones del servicio</Link>
+            </li>
+            <li>
+              <Link href="/cookie-policy">Politica de cookies</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="order-1 md:order-2 my-4">
+          <SubterraLogoSm />
+        </div>
+        <div className="order-3">
+          <ul className="flex flex-col items-center md:items-start text-center">
+            <li>
+              <Link href="/user-guide">Guía de uso</Link>
+            </li>
+            <li>
+              <Link href="/versions">Versiones y novedades</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contacto</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="flex flex-row gap-1">
         <span>Creado por @andoni.abarzuza con 💜 </span>
